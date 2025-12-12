78274aa6e9e07e63eeb763750fc68592ed6c5bd3
Nathan <viniciosnathan@gmail.com>
2025-12-12 07:04:52 -0300

    feat: add a push method into CustomArray

--- Arrays/index.js
+++ Arrays/index.js
@@ -3,6 +3,12 @@ class CustomArray {
         this.length = 0;
         this.data = {};
     }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
 }
 
 const customArray = new CustomArray();
