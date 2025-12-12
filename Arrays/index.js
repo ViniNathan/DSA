c2460e30012e24e8f0942d5a946f324b0df5fefc
Nathan <viniciosnathan@gmail.com>
2025-12-12 08:00:47 -0300

    feat: implement deleteByIndex method

--- Arrays/index.js
+++ Arrays/index.js
@@ -26,6 +26,24 @@ class CustomArray {
         delete this.data[this.length - 1];
         this.length--;
     }
+
+    deleteByIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[index + 1];
+        }
+        delete this.data[this.length - 1]
+        this.length--;
+
+    }
 }
 
 const customArray = new CustomArray();
+customArray.push("abacate");
+customArray.push("mexerica");
+customArray.push("maça");
+customArray.push("banana");
+customArray.push("teste");
+customArray.push("teste2");
+console.log(customArray)
+customArray.deleteByIndex(0)
+console.log(customArray)
