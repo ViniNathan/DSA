b31bb57f1c0c25be6881390b784d4e962866db8d
Nathan <viniciosnathan@gmail.com>
2025-12-16 07:55:21 -0300

    feat: implement DLinkedList constructor

--- Doubly Linked List/index.js
+++ Doubly Linked List/index.js
@@ -5,3 +5,12 @@ class Node {
         this.next = null;
     }
 }
+
+class DLinkedList {
+    constructor(data) {
+        const newNode = new Node(data);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+}
