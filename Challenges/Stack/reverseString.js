6ab7501a9685938e765c3b62055e85afe5daf60d
Nathan <viniciosnathan@gmail.com>
2025-12-22 08:42:46 -0300

    challenge: implement a string reversion algorithm using stack

---
+++ Challenges/Stack/reverseString.js
@@ -0,0 +1,16 @@
+const reverseString = (str) => {
+    const stack = [];
+
+    for (let char of str) {
+        stack.push(char)
+    }
+
+    let reversedString = "";
+
+    while (stack.length > 0) {
+        reversedString += stack.pop();
+    }
+    return reversedString;
+}
+
+console.log(reverseString("Test"))
