e18048b82a95e6beec0dd2439834deb0532dbf09
Nathan <viniciosnathan@gmail.com>
2025-12-23 20:03:08 -0300

    feat: implement a simple hashTable

---
+++ Hash Table/index.js
@@ -0,0 +1,34 @@
+// Hash function:
+// Acts like a translator, takin an input of any size (key)
+// and converting it into a fixed-size value (hash code)
+// that can be used as an index within the hash table's internal array
+// This process of mapping arbitrary keys to fixed-length indices is called hashing
+
+class HashTable {
+    constructor(size) {
+        this.keyMap = new Array(size);
+    }
+
+    _hashFunction(key) {
+        let sum = 0;
+
+        const PRIME_NUMBER = 31;
+
+        for (let i = 0; i < Math.min(key.length, 100); i++){
+            const charCode = key.charCodeAt(0) - 96;
+            sum = (sum + PRIME_NUMBER + charCode) % this.keyMap.length;
+        }
+
+        return sum;
+    }
+
+    set(key, value) {
+        const index = this._hashFunction(key);
+
+        if (!this.keyMap[index]) this.keyMap = [];
+
+        this.keyMap[index].push([key, value]);
+
+        return this;
+    }
+}
