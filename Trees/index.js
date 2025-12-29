f2a2e884b52d0dd6a0760a9916c149e2f584f28a
Nathan <viniciosnathan@gmail.com>
2025-12-29 20:25:13 -0300

    feat: implement a simple put algorithm into a BST

--- Trees/index.js
+++ Trees/index.js
@@ -10,7 +10,46 @@ class BST {
     constructor() {
         this.root = null;
     }
+
+    put(data) {
+        const newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let actualNode = this.root;
+
+        while (true) {
+            if (newNode.data === actualNode.data) {
+                return undefined
+            }
+
+            if (newNode.data < actualNode.data) {
+                if (actualNode.left === null) {
+                    actualNode.left = newNode;
+                    return this;
+                } else {
+                    actualNode = actualNode.left;
+                }
+                
+            } else {
+                if (actualNode.right === null) {
+                    actualNode.right = newNode;
+                    return this;
+                } else {
+                    actualNode = actualNode.right;
+                }
+            }
+        }
+    }
 }
 
 const tree = new BST();
+tree.put(1)
+tree.put(2)
+tree.put(0)
+tree.put(3)
+tree.put(-1)
 console.log(tree)
