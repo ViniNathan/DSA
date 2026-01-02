d28c5e20ab50c5fad8b908704775fa1697d21a38
Nathan <viniciosnathan@gmail.com>
2026-01-02 07:23:02 -0300

    feat: implement the addVertex method to Graph

--- Graph/index.js
+++ Graph/index.js
@@ -20,8 +20,19 @@
 class Graph {
     constructor() {
         this.adjacencyList = {}
+    };
+
+    addVertex(vtx) {
+        if (!this.adjacencyList[vtx]) {
+            this.adjacencyList[vtx] = [];
+            return true;
+        }
+        return false;
     }
 }
 
 const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B")
+graph.addVertex("C")
 console.log(graph);
