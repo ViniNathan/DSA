79051fee194717b4a3045f053c4b0a289daf85bb
Nathan <viniciosnathan@gmail.com>
2026-01-02 07:17:54 -0300

    docs: aff adjency list concept to graph

--- Graph/index.js
+++ Graph/index.js
@@ -1,2 +1,18 @@
 // Graph is a non-linear data structure that models relationships between objects.
 // It consists of two main components: Vertices (nodes) & Edges
+// Adjency list: shows to us how the Vertices are connected by Edges:
+//
+//    E-----------A--------B
+//    |                    |
+//    |                    |
+//    |                    |
+//    |                    |
+//    D--------------------C
+//
+// {
+//   A:["B","E"],
+//   B:["A", "C"].
+//   C:["B","D"],
+//   D:["C","E"],
+//   E:["A","D"]
+// }
