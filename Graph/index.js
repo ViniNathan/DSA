ecab9d4454d22ed67443d15fbb161766d449b92f
Nathan <viniciosnathan@gmail.com>
2026-01-03 18:29:57 -0300

    feat: implement remove edge to graph

--- Graph/index.js
+++ Graph/index.js
@@ -26,24 +26,40 @@ class Graph {
         if (!this.adjacencyList[vtx]) {
             this.adjacencyList[vtx] = [];
             return true;
-        }
+        };
         return false;
     };
 
-    addEdges(vtx1, vtx2) {
+    addEdge(vtx1, vtx2) {
         if (this.adjacencyList[vtx1] && this.adjacencyList[vtx2]) {
             this.adjacencyList[vtx1].push(vtx2);
             this.adjacencyList[vtx2].push(vtx1);
             return true;
-        }
+        };
 
         return false;
-    }
+    };
+
+    removeEdge(vtx1, vtx2) {
+        if (this.adjacencyList[vtx1] && this.adjacencyList[vtx2]) {
+            this.adjacencyList[vtx1] = this.adjacencyList[vtx1].filter(
+                (v) => v != vtx2
+            );
+            this.adjacencyList[vtx2] = this.adjacencyList[vtx2].filter(
+                (v) => v != vtx1
+            );
+            return true;
+        };
+
+        return false;
+    };
 }
 
 const graph = new Graph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
-graph.addEdges("A","B");
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.removeEdge("A","B");
 console.log(graph);
